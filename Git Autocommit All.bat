720ffd57e75ffc41616a4dd772c41e16fa51f0c9
Rz Rasel <Rz Rasel>
2017-12-15 04:13:27 +0600

    asdf

--- Git Autocommit All.bat
+++ Git Autocommit All.bat
@@ -1,13 +1,16 @@
-@echo off
+:: @echo off
 :: @echo off
 REM set /p input="Enter ID: "
 REM echo %input%
 REM pause
 
-@echo off
+:: @echo off
 set /p input = "Enter Commit Text: "
+set gcommand=git commit -m "%input%"
 git add .
-git commit -m "COMMIT COMPLETE"
+:: git commit -m "COMMIT COMPLETE A"
+echo %gCommand%
+%gCommand%
 git pull
 git push --all
 echo Process Complete Press Enter...
---
+++ test.bat
@@ -0,0 +1,10 @@
+@echo off
+set date=%DATE%
+set var=%date% A Date
+set /p UserInputPath=What Directory would you like?
+echo %UserInputPath%
+:: test.bat < test.bat
+:: %UserInputPath%
+:: %DATE%
+echo %var%
+pause
