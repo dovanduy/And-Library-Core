deb23d43c8311e6284728acf1e514cbcaf30ab9f
Rz Rasel <rzrasel@users.noreply.github.com>
2018-01-23 17:18:58 +0600

    Update DeviceIPApi.java

--- librarycore/src/main/java/com/rz/librarycore/DeviceIPApi.java
+++ librarycore/src/main/java/com/rz/librarycore/DeviceIPApi.java
@@ -162,6 +162,31 @@ public class DeviceIPApi {
             }
         }
     }
+    public String getMacAddress() {
+        try {
+            List<NetworkInterface> all = Collections.list(NetworkInterface.getNetworkInterfaces());
+            for (NetworkInterface nif : all) {
+                if (!nif.getName().equalsIgnoreCase("wlan0")) continue;
+
+                byte[] macBytes = nif.getHardwareAddress();
+                if (macBytes == null) {
+                    return "";
+                }
+
+                StringBuilder res1 = new StringBuilder();
+                for (byte b : macBytes) {
+                    res1.append(Integer.toHexString(b & 0xFF) + ":");
+                }
+
+                if (res1.length() > 0) {
+                    res1.deleteCharAt(res1.length() - 1);
+                }
+                return res1.toString();
+            }
+        } catch (Exception ex) {
+        }
+        return "02:00:00:00:00:00";
+    }
 
     public interface EventListenerHandler {
         public void onPostExecute(HashMap<String, String> argResult);
