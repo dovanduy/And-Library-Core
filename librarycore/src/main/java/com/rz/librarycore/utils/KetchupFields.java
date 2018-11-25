73d9ae2e2af65b15231a765f2a469648d375cbf6
Rz Rasel <developer@RzRasel.local>
2018-11-25 17:04:53 +0600

    And Library Core V-100.00.01 - 2018-11-25 17:04:53

--- librarycore/src/main/java/com/rz/librarycore/utils/KetchupFields.java
+++ librarycore/src/main/java/com/rz/librarycore/utils/KetchupFields.java
@@ -1,10 +1,9 @@
-package com.library;
+package com.rz.librarycore.utils;
 
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class KetchupFields {
